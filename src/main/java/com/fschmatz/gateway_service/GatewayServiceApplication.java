b2e9019b17cb0df8b1fc15aab46741cf2afac4be
Fschmatz <fschmatz@hotmail.com>
2021-11-08 22:49:55 -0300

    v 0.0.1

--- src/main/java/com/fschmatz/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/fschmatz/gateway_service/GatewayServiceApplication.java
@@ -5,24 +5,26 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
-    public static final String HTTP_HTTBIN_ORG = "http://httpbin.org";
+    public static final String teste = "https://google.com";
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "Coffe"))
-                        .uri(HTTP_HTTBIN_ORG + ":80"))
-                .route(p -> p.path("/ola")
-                        .filters(f -> f.addRequestHeader("Hello", "application/json"))
-                        .uri(HTTP_HTTBIN_ORG + "/json"))
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/teste")
+                        .uri(teste))
                 .build();
 
     }
