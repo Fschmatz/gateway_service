abf1f3baf37b1876473b9d30d336e002457bb980
Fschmatz <fschmatz@hotmail.com>
2021-11-11 22:09:36 -0300

    v 0.0.3
    Added Eureka
    Funcional?

--- src/main/java/com/fschmatz/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/fschmatz/gateway_service/GatewayServiceApplication.java
@@ -1,40 +1,15 @@
 package com.fschmatz.gateway_service;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GatewayServiceApplication {
-
-    public static final String teste = "http://localhost:9090/error.html";
-    public static final String teste2 = "http://localhost:9090/homeTeste.html";
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
-    }
-
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-               /* .route(p -> p
-                        .path("/get")
-                        .filters(
-                               gatewayFilterSpec -> gatewayFilterSpec.setPath(
-                                       "/ola"
-                               )
-                        )
-                        .uri("https://www.google.com/search?q="))*/
-                .route(p -> p.path("/teste")
-                        .uri(teste))
-                .route(p -> p.path("/teste2")
-                        .uri(teste2))
-                .build();
-
     }
 
 }
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,3 +1,4 @@
+/*
 
 package controller;
 
@@ -46,6 +47,7 @@ public class MainController {
 
 
 
+*/
 /* //------------------ HTML CATEGORIAS
 
     @RequestMapping("/falenciaPessoal/categorias")
@@ -106,7 +108,5 @@ public class MainController {
     }
 
     //------------------ HTML CATEGORIAS*//*
-
-
-}
 */
+
